1f6c5676c58b6dc149eea6708f507b5616d0a39f
SavaBM <128490103+SavaBM@users.noreply.github.com>
2023-05-12 15:49:43 +0500

    Create .py

---
+++ .py
@@ -0,0 +1,14 @@
+num_tickets = int(input("Введите количество билетов: "))
+total_cost = 0
+num_free_tickets = 0
+for i in range(num_tickets):
+    age = int(input("Введите возраст посетителя: "))
+    if age < 18:
+        num_free_tickets += 1
+    elif age >= 18 and age < 25:
+        total_cost += 990
+    else:
+        total_cost += 1390
+if num_tickets > 3:
+    total_cost *= 0.9
+print("Сумма к оплате:", total_cost, "руб.")
